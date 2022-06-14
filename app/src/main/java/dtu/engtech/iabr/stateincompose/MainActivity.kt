29484c5f5700118a872137cd6ce8c3388998f3e0
TestUndervisning <hansen.pauline@yahoo.dk>
2022-06-14 14:22:44 +0200

    firebase

--- app/src/main/java/dtu/engtech/iabr/stateincompose/MainActivity.kt
+++ app/src/main/java/dtu/engtech/iabr/stateincompose/MainActivity.kt
@@ -1,16 +1,15 @@
 package dtu.engtech.iabr.stateincompose
 
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import dtu.engtech.iabr.stateincompose.ui.theme.StateInComposeTheme
-
-
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +25,22 @@ class MainActivity : ComponentActivity() {
                     Navigation()
 
                 }
-
             }
         }
+        testFirestoreGet()
+    }
+
+    private fun testFirestoreGet() {
+        val docRef = FirebaseFirestore.getInstance().collection("equipment")
+        docRef.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents){
+                    Log.d("Firebase","${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener{exception ->
+                Log.w("Firebase","Error getting documents: ", exception)
+            }
     }
 }
 
